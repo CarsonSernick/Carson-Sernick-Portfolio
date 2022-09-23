c03ed8911d7a7c40255abb8bcbd7c63eebf8b9fa
Carson Sernick <50496306+Pr0H4cker@users.noreply.github.com>
2022-09-22 21:37:32 -0400

    Add files via upload
    website transferred from IFASTNET hosting.

---
+++ terminal/data.js
@@ -0,0 +1,36 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'Carson Sernick'}
+  ],
+  bio: [
+    {name: 'Name', value: 'Carson Sernick'},
+    {name: 'Email', value: 'Carson@carsonsernick.info'},
+    {name: 'Phone', value: 'n/a'},
+    {name: 'Country', value: 'United States'},
+    {name: 'City', value: 'Greenwich'}
+  ],
+  about: [
+    {name: 'Introduction', value: 'I am a Coder! Just like many of you out there. I tought myself how to code when I was about 9 years old. The reason why; I didn`t want to do work! I tried compiling some code from IXL and it did not work. I was confused, so I gave up. Some time later I went back to it, and found out there is a lot that goes into making a website. that`s how it all started'},
+    {name: '', value: ''},
+    {name: 'What I do', value: 'I code in many languages, however I have my favorites, and I some I am better than others. I code in HTML, JS, CSS; which are my main languages. I want to learn python and C Language so I can make API, more games, and robots!'},
+    {name: '', value: ''},
+    {name: 'Education', value: 'Over the summer I did this course with Columbia, it was meant for highschoolers, and at the time I was not one. I loved it there, and I was a top coder, getting things done faster than everyone else. At the end, they described me as a Little Elon Musk. (True Story). That was the time I knew I should really keep coding'},
+    {name: '', value: ''},
+    {name: 'More Coming soon!', value: 'Checkout my current portfolio, carsonsernick.info'}
+
+  ],
+  github: [
+    {name: 'GitHub', value: 'Pr0H4cker'}
+  ],
+  projects: [
+    {name: 'CarsonCraft', value: 'https://replit.com/@Skyskript/carsoncraft'},
+    {name: 'Terminal Portfolio', value: 'https://replit.com/@Skyskript/Terminal-Portfolio'},
+    {name: 'Bootstrap Portfolio', value: 'https://replit.com/@Skyskript/Bootstrap-Portfolio'},
+    {name: 'Greenwichfreetutors', value: 'https://greenwichfreetutors.com'},
+  ],
+  socials: [
+    {name: 'Repl.it', value: 'https://replit.com/@Skyskript'},
+    {name: 'GitHub', value: 'https://github.com/Pr0H4cker'},
+    {name: 'Website', value: 'https://carsonsernick.info'}
+  ]
+}
